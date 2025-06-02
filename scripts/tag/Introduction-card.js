c7fb4e760a9d22466aa60da4816b6aaca303e116
落魄君子 <luopojunzi@gmail.com>
2025-06-02 12:29:18 +0800

    fix: correct tabindex attribute in Introduction card

--- scripts/tag/Introduction-card.js
+++ scripts/tag/Introduction-card.js
@@ -28,7 +28,7 @@ function intCard(args) {
           </div>
         </div>
         <div class="right">
-          <a href="${urlFor(link)}" tableindex="-1" class="no-text-decoration">前往</a>
+          <a href="${urlFor(link)}" tabindex="-1" class="no-text-decoration">前往</a>
         </div>
       </div>
     `;
